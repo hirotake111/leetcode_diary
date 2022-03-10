a08027cf5fd9c3b06ce6d9a26b20854d4a8e367c
hirotake111 <hirotake111@users.noreply.github.com>
2022-03-11 07:15:23 +0900

    slight update

--- leetcode/add_two_numbers/solution.py
+++ leetcode/add_two_numbers/solution.py
@@ -1,17 +1,20 @@
 from typing import List, Optional
-from unittest import TestCase,main
+from unittest import TestCase, main
 
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
     def __str__(self) -> str:
         return f"{self.val}"
 
 
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         carry = 0
         entry = ListNode()
         node = entry
@@ -53,11 +56,11 @@ class Solution:
             l2 = l2.next
             node.next = ListNode()
             node = node.next
-        
+
         if carry == 1:
             node.next = ListNode()
             node.next.val = 1
-        
+
         return entry
 
 
@@ -69,8 +72,9 @@ def list_to_node(l: List[int]):
         if i != len(l) - 1:
             node.next = ListNode()
             node = node.next
-    
+
     return entry
+
 
 def printNode(l: ListNode):
     while True:
@@ -79,7 +83,8 @@ def printNode(l: ListNode):
         if l is None:
             break
 
-def node_to_list(l:ListNode):
+
+def node_to_list(l: ListNode):
     result = []
     while True:
         result.append(l.val)
@@ -89,6 +94,7 @@ def node_to_list(l:ListNode):
 
     return result
 
+
 # Input: l1 = [2,4,3], l2 = [5,6,4]
 # Output: [7,0,8]
 # Explanation: 342 + 465 = 807.
@@ -97,21 +103,20 @@ class TestSolution(TestCase):
         self.s = Solution()
 
     def test_getNode(self):
-        node = list_to_node([1,2,3])
-        self.assertEqual(node_to_list( node), [1,2,3])
+        node = list_to_node([1, 2, 3])
+        self.assertEqual(node_to_list(node), [1, 2, 3])
 
     def test_input(self):
-        l1 = list_to_node([2,4,3])
-        l2 = list_to_node([5,6,4])
+        l1 = list_to_node([2, 4, 3])
+        l2 = list_to_node([5, 6, 4])
         node = self.s.addTwoNumbers(l1, l2)
-        self.assertEqual(node_to_list(node), [7,0,8])
-    
-    def test_input2(self):
-        l1 = list_to_node([9,9,9,9,9,9,9])
-        l2 = list_to_node([9,9,9,9])
-        node = self.s.addTwoNumbers(l1, l2)
-        self.assertEqual(node_to_list(node), [8,9,9,9,0,0,0,1])
+        self.assertEqual(node_to_list(node), [7, 0, 8])
 
+    def test_input2(self):
+        l1 = list_to_node([9, 9, 9, 9, 9, 9, 9])
+        l2 = list_to_node([9, 9, 9, 9])
+        node = self.s.addTwoNumbers(l1, l2)
+        self.assertEqual(node_to_list(node), [8, 9, 9, 9, 0, 0, 0, 1])
 
 
 if __name__ == "__main__":
