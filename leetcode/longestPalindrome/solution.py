17c6ca573d2b786b1e2ef55d9b4612b5cdcb071e
hirotake111 <hirotake111@users.noreply.github.com>
2022-03-12 18:37:57 +0900

    in progress

---
+++ leetcode/longestPalindrome/solution.py
@@ -0,0 +1,59 @@
+from unittest import main, TestCase
+
+
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        l = len(s)
+        longest = ""
+
+        for i in range(l):
+            if i + 1 < l and s[i] == s[i + 1]:  # aa
+                for j in range(i + 1):
+                    substring = s[i - j : i + j + 2]
+                    print(i, j, substring, " -> ", i + j < l and s[i - j] == s[i + j])
+                    if i + j < l and s[i - j] == s[i + j]:
+                        if len(longest) < len(substring):
+                            longest = substring
+                        continue
+                    break
+
+            for j in range(i + 1):
+                if i + j >= l:
+                    break
+                # print(f"i:{i}, j:{j}")
+                substring = s[i - j : i + j + 1]
+                # print(
+                #     f"compare: s[{i}-{j}]={s[i - j]}, s[{i}+{j}]={s[i + j]}, {substring}"
+                # )
+                if i + j < l and s[i - j] == s[i + j]:
+                    if len(longest) < len(substring):
+                        # print(f"update longest: {s[i - j : i + j + 1]}")
+                        longest = substring
+                    continue
+                break
+        return longest
+
+
+class TestSolution(TestCase):
+    def setUp(self) -> None:
+        self.s = Solution()
+        return super().setUp()
+
+    def test_solution(self):
+        # self.assertIn(self.s.longestPalindrome("babad"), ["bab", "aba"])
+        # self.assertEqual(self.s.longestPalindrome("cbbd"), "bb")
+        # self.assertEqual(self.s.longestPalindrome("ccc"), "ccc")
+        # self.assertEqual(self.s.longestPalindrome("accca"), "accca")
+        # self.assertEqual(self.s.longestPalindrome("aabcdefgh"), "aa")
+        # self.assertEqual(self.s.longestPalindrome("aaaa"), "aaaa")
+        self.assertEqual(
+            self.s.longestPalindrome("abbcccbbbcaaccbababcbcabca"), "bbcccbb"
+        )
+
+
+if __name__ == "__main__":
+    main()
