7d1667603d710a972a2be6b8d0f6074edbabe0c6
hirotake111 <hirotake111@users.noreply.github.com>
2022-03-14 07:11:35 +0900

    container_with_most_water complete

---
+++ leetcode/container_With_Most_Water/solution.py
@@ -0,0 +1,31 @@
+from unittest import main, TestCase
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area, i, j = 0, 0, len(height) - 1
+        while i < j:
+            # calc area
+            area = min(height[i], height[j]) * (j - i)
+            # update max_area
+            if area > max_area:
+                max_area = area
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+
+        return max_area
+
+
+class TestSolution(TestCase):
+    s = Solution()
+
+    def test_solution(self):
+        # self.assertEqual(self.s.maxArea([1, 1]), 1)
+        self.assertEqual(self.s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
+
+
+if __name__ == "__main__":
+    main()
