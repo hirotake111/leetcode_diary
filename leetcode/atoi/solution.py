baa1b86252f055de89b83ae4231e30cd6356606c
hirotake111 <hirotake111@users.noreply.github.com>
2022-03-13 17:45:08 +0900

    atoi complete

---
+++ leetcode/atoi/solution.py
@@ -0,0 +1,43 @@
+from unittest import main, TestCase
+
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        result = ""
+        n = 1
+        s = s.strip()
+
+        if s == "":
+            return 0
+        if s[0] in "-+":
+            if s[0] == "-":
+                n = -1
+            s = s[1:]
+
+        for c in s:
+            if c not in "1234567890":
+                break
+            result += c
+        if result == "":
+            return 0
+        result = int(result) * n
+        if result < -(2**31):
+            return -(2**31)
+        if result > 2**31 - 1:
+            return 2**31 - 1
+        return result
+
+
+class TestSolution(TestCase):
+    def test_solution(self):
+        s = Solution()
+        self.assertEqual(s.myAtoi("123"), 123)
+        self.assertEqual(s.myAtoi("0032"), 32)
+        self.assertEqual(s.myAtoi("-123"), -123)
+        self.assertEqual(s.myAtoi("-+12"), 0)
+        self.assertEqual(s.myAtoi("number is 12"), 0)
+        self.assertEqual(s.myAtoi("   +0 123"), 0)
+
+
+if __name__ == "__main__":
+    main()
