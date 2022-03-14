ddc5cf8bf2cbbe4176ac28f8164dde8459237e97
hirotake111 <hirotake111@users.noreply.github.com>
2022-03-15 06:11:54 +0900

    roman_to_int complete

--- leetcode/roman_to_integer/solution.py
+++ leetcode/roman_to_integer/solution.py
@@ -8,58 +8,92 @@ L             50
 C             100
 D             500
 M             1000
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+CM: 900
+CD: 400
+XC: 90
+XL: 40
+IX: 9
+IV: 4
 """
 
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        prev = ""
         result = 0
-        for c in s:
-            if c == "M":
-                if prev == "C":
-                    result += 900
-                else:
-                    result += 1000
-            if c == "D":
-                if prev == "C":
-                    # 400
-                    result += 400
-                else:
-                    result += 500
-            if c == "C":
-                if prev == "C":
-                    result += 200
-                elif prev == "X":
-                    result += 90
-                else:
-                    result += 100
-            if c == "L":
-                if prev == "C":
-                    result += 100
-                if prev == "X":
-                    # 40
-                    result += 40
-                else:
-                    result += 50
-            if c == "X":
-                if prev == "X":
-                    result += 20
-                elif prev == "I":
-                    result += 9
-                else:
-                    result += 10
-            if c == "V":
-                if prev == "I":
-                    result += 4
-                else:
-                    result += 5
-            if c == "I":
-                if prev == "I":
-                    result += 1
-            prev = c
-        if prev == "I":
+
+        # 0
+        if len(s) == 0:
+            return 0
+
+        # 1000
+        while len(s) > 0 and s[0] == "M":
+            s = s[1:]
+            result += 1000
+
+        # 900
+        if len(s) > 1 and s[:2] == "CM":
+            s = s[2:]
+            result += 900
+
+        # 400
+        if len(s) > 1 and s[:2] == "CD":
+            s = s[2:]
+            result += 400
+
+        # 500
+        if len(s) > 0 and s[0] == "D":
+            s = s[1:]
+            result += 500
+
+        # 100
+        while len(s) > 0 and s[0] == "C":
+            s = s[1:]
+            result += 100
+
+        # 90
+        if len(s) > 1 and s[:2] == "XC":
+            s = s[2:]
+            result += 90
+
+        # 40
+        if len(s) > 1 and s[:2] == "XL":
+            s = s[2:]
+            result += 40
+
+        # 50
+        if len(s) > 0 and s[0] == "L":
+            s = s[1:]
+            result += 50
+
+        # 10
+        while len(s) > 0 and s[0] == "X":
+            s = s[1:]
+            result += 10
+
+        # 9
+        if len(s) > 1 and s[:2] == "IX":
+            s = s[2:]
+            result += 9
+
+        # 4
+        if len(s) > 1 and s[:2] == "IV":
+            s = s[2:]
+            result += 4
+
+        # 5
+        if len(s) > 0 and s[0] == "V":
+            s = s[1:]
+            result += 5
+
+        # 1
+        while len(s) > 0 and s[0] == "I":
+            s = s[1:]
             result += 1
+
         return result
 
 
@@ -67,19 +101,24 @@ class TestSolution(TestCase):
     s = Solution()
 
     def test_solution(self):
-        # self.assertEqual(self.s.romanToInt("III"), 3)
-        # self.assertEqual(self.s.romanToInt("LVIII"), 58)
-        # self.assertEqual(self.s.romanToInt("MCMXCIV"), 1994)
-        # self.assertEqual(self.s.romanToInt("XXVII"), 27)
-        # self.assertEqual(self.s.romanToInt("XIX"), 19)
-        # self.assertEqual(self.s.romanToInt("XL"), 40)
-        # self.assertEqual(self.s.romanToInt("XLIX"), 49)
-        # self.assertEqual(self.s.romanToInt("L"), 50)
-        # self.assertEqual(self.s.romanToInt("LI"), 51)
-        # self.assertEqual(self.s.romanToInt("DCXXI"), 621)
-        # self.assertEqual(self.s.romanToInt("MMCDXXV"), 2425)
-        # self.assertEqual(self.s.romanToInt("IX"), 9)
+        self.assertEqual(self.s.romanToInt("III"), 3)
+        self.assertEqual(self.s.romanToInt("LVIII"), 58)
+        self.assertEqual(self.s.romanToInt("MCMXCIV"), 1994)
+        self.assertEqual(self.s.romanToInt("XXVII"), 27)
+        self.assertEqual(self.s.romanToInt("XIX"), 19)
+        self.assertEqual(self.s.romanToInt("XL"), 40)
+        self.assertEqual(self.s.romanToInt("XLIX"), 49)
+        self.assertEqual(self.s.romanToInt("L"), 50)
+        self.assertEqual(self.s.romanToInt("LI"), 51)
+        self.assertEqual(self.s.romanToInt("DCXXI"), 621)
+        self.assertEqual(self.s.romanToInt("MMCDXXV"), 2425)
+        self.assertEqual(self.s.romanToInt("IX"), 9)
         self.assertEqual(self.s.romanToInt("MDLXX"), 1570)
+        self.assertEqual(self.s.romanToInt(""), 0)
+        self.assertEqual(self.s.romanToInt("MCM"), 1900)
+        self.assertEqual(self.s.romanToInt("MCD"), 1400)
+        self.assertEqual(self.s.romanToInt("MD"), 1500)
+        self.assertEqual(self.s.romanToInt("MDCCC"), 1800)
 
 
 if __name__ == "__main__":
