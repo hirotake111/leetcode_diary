b9fe00690d3777a6d87a3d0450eb66d6ce6611e5
hirotake111 <hirotake111@users.noreply.github.com>
2022-03-17 06:38:34 +0900

    longest common prefix complete

--- leetcode/longest_common_prefix/solution.py
+++ leetcode/longest_common_prefix/solution.py
@@ -1,20 +1,42 @@
 from typing import List
+from unittest import main, TestCase
 
 """
 abcdedf
 ccbcdffff
 """
 
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        result = ""
-        length = len(strs)
-        if len(strs) == 1:
-            return strs[0]
-        if len()
+        string = None
+        for s in strs:
+            if string == None:
+                string = s
+                continue
+            tmp = ""
+            for x, y in zip(string, s):
+                if x == y:
+                    tmp += x
+                else:
+                    break
+            string = tmp
+
+        return string
+
+
+class TestSolution(TestCase):
+    s = Solution()
+
+    def test_solution(self):
+        self.assertEqual(self.s.longestCommonPrefix(["flower", "flow", "flight"]), "fl")
+        self.assertEqual(self.s.longestCommonPrefix(["dog", "racecar", "car"]), "")
+        self.assertEqual(self.s.longestCommonPrefix(["cir", "car"]), "c")
 
 
 if __name__ == "__main__":
-    s = Solution()
-    assert s.longestCommonPrefix(["flower", "flow", "flight"]) == "fl"
+    main()
+    # s = Solution()
+    # assert s.longestCommonPrefix(["flower", "flow", "flight"]) == "fl"
     # assert s.longestCommonPrefix(["dog", "racecar", "car"]) == ""
+    # assert s.longestCommonPrefix(["cir", "car"]) == ""
