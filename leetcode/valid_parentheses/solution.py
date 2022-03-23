8e2797d39a6ac2e6b125d3dd4230e62fe5de19ca
hirotake111 <hirotake111@users.noreply.github.com>
2022-03-24 05:45:31 +0900

    valid parentheses complete

---
+++ leetcode/valid_parentheses/solution.py
@@ -0,0 +1,38 @@
+from unittest import main, TestCase
+
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+"""
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        m = {")": "(", "}": "{", "]": "["}
+        for c in s:
+            if c in [")", "}", "]"]:
+                if stack[-1] != m[c]:
+                    return False
+                stack.pop()
+                continue
+            stack.append(c)
+
+        return True if len(stack) == 0 else False
+
+
+class TestSolution(TestCase):
+    s = Solution()
+
+    def test_solution(self):
+        self.assertEqual(self.s.isValid(s="()"), True)
+        self.assertEqual(self.s.isValid(s="()[]{}"), True)
+        self.assertEqual(self.s.isValid(s="(]"), False)
+
+
+if __name__ == "__main__":
+    main()
