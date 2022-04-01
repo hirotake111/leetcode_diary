78623a86d4d19c0c9ae47f7171ff532e2d5fcbca
hirotake111 <hirotake111@users.noreply.github.com>
2022-04-01 15:33:09 +0900

    count and say complete

---
+++ leetcode/count_and_say/s.py
@@ -0,0 +1,48 @@
+from collections import Counter
+from unittest import main, TestCase
+
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        ans = "1"
+        while n > 1:
+            tmp = ""
+            current = None
+            count = 0
+            for c in ans:
+                if current is None:  # set current
+                    current = c
+                    count += 1
+                    continue
+                if c == current:  # count up current
+                    count += 1
+                    continue
+                # c is different from current
+                # concatenate current and count to answer
+                tmp += str(count) + current
+                #  go to next character
+                current = c
+                count = 1
+            # coancatenate current and count to answer
+            tmp += str(count) + current
+            n -= 1
+            ans = tmp
+
+        return ans
+
+
+class TestSolution(TestCase):
+    s = Solution()
+
+    def test_s(self):
+        self.assertEqual(self.s.countAndSay(1), "1")
+        self.assertEqual(self.s.countAndSay(2), "11")
+        self.assertEqual(self.s.countAndSay(3), "21")
+        self.assertEqual(self.s.countAndSay(4), "1211")
+        self.assertEqual(self.s.countAndSay(5), "111221")
+        self.assertEqual(self.s.countAndSay(6), "312211")
+        self.assertEqual(self.s.countAndSay(7), "13112221")
+
+
+if __name__ == "__main__":
+    main()
