2b257e2fe6f33b18163ae7cddfe21db489fc374c
hirotake111 <hirotake111@users.noreply.github.com>
2022-05-01 17:53:57 +0900

    updated backspace string compare

--- leetcode/backspace_string_compare/solution.py
+++ leetcode/backspace_string_compare/solution.py
@@ -1,42 +1,42 @@
 from typing import List, Tuple
 from unittest import main, TestCase
 
-"ab##"
-"a#b#"
-"a#b"
-"ab#"
-
 
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        i, j = len(s) - 1, len(t) - 1
-        while i >= 0 or j >= 0:
+        si, ti = len(s) - 1, len(t) - 1
+        while si >= 0 or ti >= 0:
             sskip, tskip = 0, 0
-            while i >= 0 and (s[i] == "#" or sskip != 0):
-                if s[i] == "#":
-                    i -= 1
+            while si >= 0 and s[si] == "#":
+                while s[si] == "#":
+                    si -= 1
                     sskip += 1
-                else:
-                    i -= sskip
-                    sskip = 0
+                while sskip > 0:
+                    si -= 1
+                    sskip -= 1
+                    if s[si] == "#" or si < 0:
+                        break
 
-            while j >= 0 and (t[j] == "#" or tskip != 0):
-                if t[j] == "#":
-                    j -= 1
+            while ti >= 0 and t[ti] == "#":
+                while t[ti] == "#":
+                    ti -= 1
                     tskip += 1
-                else:
-                    j -= tskip
-                    tskip = 0
+                while tskip > 0:
+                    ti -= 1
+                    tskip -= 1
+                    if t[ti] == "#" or ti < 0:
+                        break
 
-            if i < 0 or j < 0:
+            if si < 0 and ti < 0:
                 return True
-            if i == -1 or j == -1:
+            if si == -1 or ti == -1:
                 return False
-            if s[i] != t[j]:
+            if s[si] != t[ti]:
                 return False
 
-            i -= 1
-            j -= 1
+            si -= 1
+            ti -= 1
+
         return True
 
 
@@ -44,6 +44,9 @@ class TestSolution(TestCase):
     s = Solution()
 
     def test_solution(self):
+        # s = "a#b#c#"
+        # t = "c#d#"
+        # self.assertEqual(self.s.backspaceCompare(s, t), True)
         s = "ab##"
         t = "c#d#"
         self.assertEqual(self.s.backspaceCompare(s, t), True)
