d0e75a1f14457d158d919d70cc973321819d41c2
hirotake111 <hirotake111@users.noreply.github.com>
2022-06-24 16:42:50 +0900

    updated construct target array with multiple sums

--- leetcode/construct_target_array_with_multiple_sums/solution.py
+++ leetcode/construct_target_array_with_multiple_sums/solution.py
@@ -1,4 +1,5 @@
 from audioop import reverse
+from heapq import heapify, heappop, heappush
 from tkinter import W
 from typing import List, Tuple
 from unittest import TestCase, main
@@ -6,9 +7,11 @@ from unittest import TestCase, main
 
 class Solution:
     def isPossible(self, target: List[int]) -> bool:
-        target.sort(reverse=True)
+        heap = [-n for n in target]
+        heapify(heap)
         summary = sum(target)
         l = len(target)
+        expected = [-1] * l
 
         if l == 0:
             return False
@@ -17,35 +20,31 @@ class Solution:
             return True if target[0] == 1 else False
 
         if l == 2:
-            if target[0] % target[1] == 0 and target[1] != 1:
+            large, small = max(target), min(target)
+            if large % small == 0 and small != 1:
                 return False
             return True
 
         while True:
-            n = target[0]
+            n = -heappop(heap)
             summary -= n
             if n <= summary:
                 return False
 
             n %= summary
 
-            if n == 1:
-                target = target[1:] + [1]
-                summary += 1
-                if target == [1] * l:
-                    return True
-            else:
-                target[0] = n
-                target.sort(reverse=True)
-                summary += n
+            heappush(heap, -n)
+            if heap == expected:
+                return True
+            summary += n
 
 
 class Test(TestCase):
     s = Solution()
     data: List[Tuple[List[int], bool]] = [
+        ([1, 1, 999999999], True),
         ([8, 5], True),
         ([2, 900000002], False),
-        ([1, 1, 999999999], True),
         ([1, 1000000000], True),
         ([9, 3, 5], True),
         ([1, 1, 1, 2], False),
