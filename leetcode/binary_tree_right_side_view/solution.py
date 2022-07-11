590fc8170d55577b221ac3613011e968cae3e2fb
hirotake111 <hirotake111@users.noreply.github.com>
2022-07-12 07:02:37 +0900

    binary tree right side view complete

---
+++ leetcode/binary_tree_right_side_view/solution.py
@@ -0,0 +1,35 @@
+from queue import Queue
+from tkinter import W
+from typing import Optional, List, Tuple
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        values: List[int] = []
+        length: int = 0
+        queue: Queue[Tuple[TreeNode, int]] = Queue()
+
+        if root is None:
+            return []
+
+        queue.put((root, 0))
+        while not queue.empty():
+            (node, level) = queue.get()
+            if level >= length:
+                values.append(node.val)
+                length += 1
+            else:
+                values[level] = node.val
+            if node.left:
+                queue.put((node.left, level + 1))
+            if node.right:
+                queue.put((node.right, level + 1))
+
+        return values
