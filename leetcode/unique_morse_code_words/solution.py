668cc6f08ace010a54028eb38eef402bf109de98
hirotake111 <hirotake111@users.noreply.github.com>
2022-08-18 06:58:01 +0900

    unique morse code words complete

---
+++ leetcode/unique_morse_code_words/solution.py
@@ -0,0 +1,39 @@
+from typing import Set, List
+
+codes = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+}
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        s: Set[str] = set()
+        for word in words:
+            s.add("".join([codes[ch] for ch in word]))
+
+        return len(s)
