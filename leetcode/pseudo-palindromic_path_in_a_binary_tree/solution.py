688f7bc3ad476e160a82fb21608d62106bcfc76d
hirotake111 <hirotake111@users.noreply.github.com>
2022-09-15 18:30:29 +0900

    pseudo palindromic path in a binary tree complete

---
+++ leetcode/pseudo-palindromic_path_in_a_binary_tree/solution.py
@@ -0,0 +1,15 @@
+from typing import Optional
+from unittest import TestCase
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
+        pass
