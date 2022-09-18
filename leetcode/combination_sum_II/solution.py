aa212e75bc9b3e6e850c7f69ea160f03ed2904a5
hirotake111 <hirotake111@users.noreply.github.com>
2022-09-18 18:38:04 +0900

    combination sum II complete

--- leetcode/combination_sum_II/solution.py
+++ leetcode/combination_sum_II/solution.py
@@ -1,23 +1,29 @@
+from collections import defaultdict
+import enum
 from itertools import combinations
-from typing import List, Tuple
+from typing import DefaultDict, Dict, List, Tuple
 from unittest import TestCase, main
 
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        l = len(candidates)
-        filtered: List[int] = []
-        combinations: List[List[int]] = []
-        for candidate in candidates:
-            if candidate == target:
-                combinations.append([candidate])
-            elif candidate < target:
-                filtered.append(candidate)
+        answer = []
+        n = len(candidates)
+        candidates.sort()
 
-        filtered.sort()
+        def dfs(target: int, idx: int, path):
+            if target <= 0:
+                if target == 0:
+                    answer.append(path)
+                return
 
-        for i in range(l):
-            sub_candidates: List[List[int]] = []
+            for i in range(idx, n):
+                if i > idx and candidates[i] == candidates[i - 1]:
+                    continue
+                dfs(target - candidates[i], i + 1, path + [candidates[i]])
+
+        dfs(target, 0, [])
+        return answer
 
 
 class Test(TestCase):
