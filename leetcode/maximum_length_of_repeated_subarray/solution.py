e3f45ee82f6a036fcb848d6f956adf69b73fbafd
hirotake111 <hirotake111@users.noreply.github.com>
2022-09-21 08:02:30 +0900

    maximum length of repeated subarray complete

---
+++ leetcode/maximum_length_of_repeated_subarray/solution.py
@@ -0,0 +1,32 @@
+from typing import List, Tuple
+from unittest import TestCase, main
+
+
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        if nums1 == nums2:
+            return len(nums1)
+        n1, n2 = len(nums1), len(nums2)
+        dp: List[List[int]] = [[0] * (n2 + 1) for _ in nums1 + [0]]
+        for i in range(n1 - 1, -1, -1):
+            for j in range(n2 - 1, -1, -1):
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = dp[i + 1][j + 1] + 1
+
+        return max([max(arr) for arr in dp])
+
+
+class Test(TestCase):
+    data: List[Tuple[List[int], List[int], int]] = [
+        ([1, 2, 3, 2, 1], [3, 2, 1, 4, 7], 3),
+        ([0, 0, 0, 0, 0], [0, 0, 0, 0, 0], 5),
+    ]
+
+    def test_solution(self):
+        s = Solution()
+        for nums1, nums2, expected in self.data:
+            self.assertEqual(s.findLength(nums1, nums2), expected)
+
+
+if __name__ == "__main__":
+    main()
