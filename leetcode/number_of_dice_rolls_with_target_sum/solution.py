8aa2369f3b51c3d9b4b4134402baba15fef1e0e8
hirotake111 <hirotake111@users.noreply.github.com>
2022-10-03 06:50:48 +0900

    number of dice rolls wiht target sum complete

---
+++ leetcode/number_of_dice_rolls_with_target_sum/solution.py
@@ -0,0 +1,42 @@
+from typing import List, Tuple
+from unittest import TestCase, main
+
+
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        memo = [[-1] * (target + 1) for _ in range(n + 1)]
+
+        if n * k < target:
+            return 0
+
+        def dfs(idx: int, rest: int) -> int:
+            if rest <= 0:
+                return 0
+
+            if memo[idx][rest] != -1:
+                return memo[idx][rest]
+
+            if idx == n - 1:
+                return 1 if rest <= k else 0
+
+            memo[idx][rest] = sum([dfs(idx + 1, rest - j) for j in range(1, k + 1)])
+            return memo[idx][rest]
+
+        return dfs(0, target) % 1000000007
+
+
+class Test(TestCase):
+    data_set: List[Tuple[int, int, int, int]] = [
+        (1, 6, 3, 1),
+        (2, 6, 7, 6),
+        (30, 30, 500, 222616187),
+    ]
+
+    def test_solution(self):
+        s = Solution()
+        for n, k, target, expected in self.data_set:
+            self.assertEqual(s.numRollsToTarget(n, k, target), expected)
+
+
+if __name__ == "__main__":
+    main()
