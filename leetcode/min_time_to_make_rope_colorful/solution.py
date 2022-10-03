5979e72fb644539be030c6fe0364a11386034bd7
hirotake111 <hirotake111@users.noreply.github.com>
2022-10-04 07:47:17 +0900

    minimum time to make rope colorful complete

---
+++ leetcode/min_time_to_make_rope_colorful/solution.py
@@ -0,0 +1,46 @@
+from typing import List, Tuple
+from unittest import TestCase, main
+
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n = len(colors)
+
+        # edge case
+        if n < 1:
+            return 0
+
+        # at this point we are sure colors has at least 2 elements
+        answer, prev, cur = 0, 0, 1
+
+        while cur < n:
+            max_time = neededTime[prev]
+            while cur < n and colors[cur] == colors[prev]:
+                max_time = max(max_time, neededTime[cur])
+                cur += 1
+
+            if cur != prev + 1:
+                answer += sum(neededTime[prev:cur]) - max_time
+                prev = cur - 1
+            else:
+                prev, cur = prev + 1, cur + 1
+
+        return answer
+
+
+class Test(TestCase):
+    data_set: List[Tuple[str, List[int], int]] = [
+        ("bbbaaa", [4, 9, 3, 8, 8, 9], 23),
+        ("abaac", [1, 2, 3, 4, 5], 3),
+        ("abc", [1, 2, 3], 0),
+        ("aabaa", [1, 2, 3, 4, 1], 2),
+    ]
+
+    def test_solution(self):
+        s = Solution()
+        for colors, time, expected in self.data_set:
+            self.assertEquals(s.minCost(colors, time), expected)
+
+
+if __name__ == "__main__":
+    main()
