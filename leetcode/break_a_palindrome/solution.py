6b5e9c30bdfd45ac03e7d40435fb67d4dac1b66e
hirotake111 <hirotake111@users.noreply.github.com>
2022-10-11 08:22:40 +0900

    break a palindrome complete

---
+++ leetcode/break_a_palindrome/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        for i in range(len(palindrome) // 2):
+            if palindrome[i] != "a":
+                return palindrome[:i] + "a" + palindrome[i + 1 :]
+        return palindrome[:-1] + "b" if 2 <= len(palindrome) else ""
