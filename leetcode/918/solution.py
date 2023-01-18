9b2b090a40b38a1f7b4f0ae86251ba3229e02fc2
hirotake111 <hirotake111@users.noreply.github.com>
2023-01-19 07:33:50 +0900

    #918 complete

---
+++ leetcode/918/solution.py
@@ -0,0 +1,61 @@
+"""
+https://leetcode.com/problems/maximum-sum-circular-subarray/
+918. Maximum Sum Circular Subarray
+
+Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+
+A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+"""
+from typing import List, Tuple
+from unittest import TestCase, main
+
+
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        i, answer = 0, nums[0]
+        summary = sum(nums)
+
+        # short cuts
+        if 0 <= min(nums):
+            return summary
+        elif max(nums) <= 0:
+            return max(nums)
+
+        total = 0
+        while i < len(nums):
+            if total < 0:
+                # Ignore and reset previous total
+                total = 0
+            total += nums[i]
+            answer = max(answer, total)
+            i += 1
+
+        # Instead of seeing nums as circular array,
+        # let's find a minimum subarray
+        i, total = 0, 0
+        while i < len(nums):
+            if 0 < total:
+                # Ignore and reset previous total
+                total = 0
+            total += nums[i]
+            answer = max(answer, summary - total)
+            i += 1
+
+        return answer
+
+
+class Test(TestCase):
+    data: List[Tuple[List[int], int]] = [
+        ([5, -3, 5], 10),
+    ]
+
+    def test_solution(self):
+        s = Solution()
+        for nums, expected in self.data:
+            self.assertEqual(s.maxSubarraySumCircular(nums), expected)
+
+
+if __name__ == "__main__":
+    main()
