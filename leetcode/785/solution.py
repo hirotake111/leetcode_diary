aab3f195417ff4cd2bcada0f5c13076735b9d730
hirotake111 <hirotake111@users.noreply.github.com>
2023-05-20 17:39:57 +0900

    #785 complete

---
+++ leetcode/785/solution.py
@@ -0,0 +1,44 @@
+"""
+https://leetcode.com/problems/is-graph-bipartite/
+785. Is Graph Bipartite?
+There is an undirected graph with n nodes, where each node is numbered between 0 and n - 1. You are given a 2D array graph, where graph[u] is an array of nodes that node u is adjacent to. More formally, for each v in graph[u], there is an undirected edge between node u and node v. The graph has the following properties:
+
+There are no self-edges (graph[u] does not contain u).
+There are no parallel edges (graph[u] does not contain duplicate values).
+If v is in graph[u], then u is in graph[v] (the graph is undirected).
+The graph may not be connected, meaning there may be two nodes u and v such that there is no path between them.
+A graph is bipartite if the nodes can be partitioned into two independent sets A and B such that every edge in the graph connects a node in set A and a node in set B.
+
+Return true if and only if it is bipartite.
+"""
+from typing import List, Dict
+
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        """DFS approach"""
+        visited: Dict[int, bool] = {}
+
+        def dfs(i: int) -> bool:
+            for node in graph[i]:
+                if node not in visited:
+                    # Not visited
+                    visited[node] = not visited[i]
+                    if not dfs(node):
+                        return False
+                # Visited
+                # If current node and the parent node has the same boolean value,
+                # Then there is no way to partition the nodes into two independent sets
+                elif visited[node] == visited[i]:
+                    return False
+
+            return True
+
+        for i in range(len(graph)):
+            # If we haven't visited i, this means it's the beginning of another island
+            # So just mark visited[i] as True
+            if i not in visited:
+                visited[i] = True
+                if not dfs(i):
+                    return False
+        return True
