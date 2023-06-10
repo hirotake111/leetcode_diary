c1e018d43903b788733718229857af7fb7943ba3
hirotake111 <hirotake111@users.noreply.github.com>
2023-06-10 12:54:02 +0900

    #1802 complete

---
+++ 1802/solution.py
@@ -0,0 +1,35 @@
+"""
+https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/
+1802. Maximum Value at a Given Index in a Bounded Array
+
+You are given three positive integers: n, index, and maxSum. You want to construct an array nums (0-indexed) that satisfies the following conditions:
+
+nums.length == n
+nums[i] is a positive integer where 0 <= i < n.
+abs(nums[i] - nums[i+1]) <= 1 where 0 <= i < n-1.
+The sum of all the elements of nums does not exceed maxSum.
+nums[index] is maximized.
+Return nums[index] of the constructed array.
+
+Note that abs(x) equals x if x >= 0, and -x otherwise.
+"""
+
+
+class Solution:
+    def maxValue(self, n: int, index: int, maxSum: int) -> int:
+        def test(a):
+            b = max(a - index, 0)
+            res = (a + b) * (a - b + 1) // 2
+            b = max(a - ((n - 1) - index), 0)
+            res += (a + b) * (a - b + 1) // 2
+            return res - a
+
+        maxSum -= n
+        left, right = 0, maxSum
+        while left < right:
+            mid = (left + right + 1) // 2
+            if test(mid) <= maxSum:
+                left = mid
+            else:
+                right = mid - 1
+        return left + 1
