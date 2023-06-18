9ae77c55272db59742de8762b413712e86f5a602
hirotake111 <hirotake111@users.noreply.github.com>
2023-06-19 07:23:31 +0900

    update: change file structure

--- leetcode/2583/solution.rs
+++ leetcode/2583/solution.rs
@@ -22,40 +22,51 @@ use std::cell::RefCell;
 use std::cmp::Reverse;
 use std::collections::{BinaryHeap, VecDeque};
 use std::rc::Rc;
+
 impl Solution {
     pub fn kth_largest_level_sum(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i64 {
+        // Define queue and priority queue (heap)
         let mut queue: VecDeque<Rc<RefCell<TreeNode>>> = VecDeque::new();
         let mut heap: BinaryHeap<Reverse<i64>> = BinaryHeap::new();
         let k = k as usize;
 
+        // Push the root into the queue
         if let Some(node) = root {
             queue.push_back(Rc::clone(&node));
         } else {
             return -1;
         }
 
-        let mut node_count = queue.len();
+        // Get the level sum for each level
         let mut total: i64 = 0;
+        let mut node_counts = 1;
         while let Some(node) = queue.pop_front() {
-            node_count -= 1;
+            node_counts -= 1;
             let node = node.borrow();
             total += node.val as i64;
-            if let Some(node) = &node.left {
-                queue.push_back(Rc::clone(node));
+            // Add child node if exists to the queue
+            if let Some(child) = &node.left {
+                queue.push_back(Rc::clone(child));
             }
-            if let Some(node) = &node.right {
-                queue.push_back(Rc::clone(node));
+            if let Some(child) = &node.right {
+                queue.push_back(Rc::clone(child));
             }
-            if node_count == 0 {
+            // Check to see if the current node is the last one in the level
+            if node_counts == 0 {
+                // Push the total into the heap
                 heap.push(Reverse(total));
+                // reset total
                 total = 0;
-                node_count = queue.len();
-                if k < heap.len() {
+                // re-initialize node counts
+                node_counts = queue.len();
+                // if needed remove one item from the heap
+                if heap.len() > k {
                     heap.pop();
                 }
             }
         }
 
+        // If the length of the queue is less than k, return -1
         if heap.len() == k {
             heap.pop().unwrap().0
         } else {
