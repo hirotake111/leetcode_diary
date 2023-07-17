3c5cfe8995115ce0100493ce0c617e5ac71540c4
hirotake111 <hirotake111@users.noreply.github.com>
2023-07-17 17:09:07 +0900

    #445 update

--- leetcode/445/solution.py
+++ leetcode/445/solution.py
@@ -6,7 +6,7 @@ You are given two non-empty linked lists representing two non-negative integers.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 """
-from typing import Optional, List
+from typing import Optional
 
 
 # Definition for singly-linked list.
@@ -20,38 +20,25 @@ class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        arr1, arr2 = self.get_arr(l1), self.get_arr(l2)
-        diff = len(arr2) - len(arr1)
-        if diff < 0:
-            # arr1 is longer than arr2
-            arr1, arr2 = arr2, arr1
-        # Now arr2 is longer than arr1 -> perform padding
-        arr1 = [0] * abs(diff) + arr1
-        # Add arr1 + arr2
-        carry_over = 0
-        for i in range(len(arr1) - 1, -1, -1):
-            n, r = divmod(arr1[i] + arr2[i] + carry_over, 10)
-            arr1[i] = r
-            carry_over = n
-        if carry_over:
-            arr1 = [1] + arr1
-        # Create a new linked list
-        root = prev = None
-        for n in arr1:
-            node = ListNode(n)
-            if root is None:
-                root = prev = node
-            else:
-                prev.next = node
-                prev = node
+        summary = self.calc(l1) + self.calc(l2)
 
-        return root
+        # Edge case
+        if summary == 0:
+            return ListNode(0)
 
-    def get_arr(self, node: ListNode) -> List[int]:
-        arr = []
-        while True:
-            if node is None:
-                break
-            arr.append(node.val)
+        # Create a new linked list from tail to head
+        node = prev = None
+        while summary:
+            summary, remainder = divmod(summary, 10)
+            node = ListNode(remainder)
+            node.next = prev
+            prev = node
+
+        return node
+
+    def calc(self, node: Optional[ListNode]) -> int:
+        result = 0
+        while node:
+            result = result * 10 + node.val
             node = node.next
-        return arr
+        return result
