beff2a4ea2c1384d8e03d76d5355e83bb8374a57
hirotake111 <hirotake111@users.noreply.github.com>
2023-10-10 12:06:41 +0900

    #2009 more optimized solution

--- leetcode/2009/solution.py
+++ leetcode/2009/solution.py
@@ -8,23 +8,24 @@ from typing import List
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         l = len(nums)
-        boundary = l - 1
-        # remove duplicated elements to make the problem easier
-        nums = sorted(set(nums))
-        min_ops = 100001
+        # Remove duplicated elements to make the problem simple
+        nums = list(set(nums))
 
         # Shortcut
-        if max(nums) - min(nums) <= boundary:
+        if max(nums) - min(nums) <= l - 1:
+            # Add duplicated elements to the answer
             return 0 + (l - len(nums))
 
-        i = j = 0
-        while j < len(nums):
+        nums.sort()
+        min_ops = 100001
+
+        i = 0
+        for j, n in enumerate(nums):
             # Move i until we meet the condition (nums[j] - nums[i] <= boundary)
-            while i < j and nums[j] - nums[i] > boundary:
+            while i < j and n - nums[i] > l - 1:
                 i += 1
             num_of_elements = j - i + 1
             ops_needed = len(nums) - num_of_elements
             min_ops = min(min_ops, ops_needed)
-            j += 1
 
         return min_ops + (l - len(nums))
